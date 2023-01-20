9952ff02a2a6e6d0681712c6bb32f9fcf60f305e
Angelo Santos <axngrsantos@gmail.com>
2023-01-20 17:28:24 +0800

    Make minor styling on marked characters

--- src/components/HitBox.tsx
+++ src/components/HitBox.tsx
@@ -12,14 +12,15 @@ type HitBoxProps = {
 export default function Box(props:HitBoxProps) {
 	
 	const styles = {
-		border: props.isMarked ? "4px solid red" : "none",
+		border: props.isMarked ? "0.4vw solid red" : "none",
 		top:`${props.YCoordinate}vw`,
 		left:`${props.XCoordinate}vw`
 	}
 
 	return (
 		<div className="hitbox" style={styles} onClick={props.markCharacter} >
-			<h3>{props.isMarked ? `${props.name}` : ''}</h3>
+			{props.isMarked &&  <h3>{props.name}</h3>}
+
 		</div>
 	)
 }
