e31bbdbf7c0f746c15dcaafaee1cdd27339be569
Angelo Santos <axngrsantos@gmail.com>
2023-01-20 17:29:47 +0800

    Make minor change for Intro component and update css

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -10,15 +10,14 @@ export default function Intro(props:any) {
 			<div className="filler"></div>
 			<div className="intro-modal">
 				<h2>
-					Tag these characters as fast as you can. Characters that are tagged will be marked by a red circle with their name on it. Players with the shortest
-					time will have their names listed in the leaderboards.
+					Tag these characters as fast as you can. Characters that are tagged will be marked by a red circle with their name on it. Earn a spot in the top 10 by finishing the game as quickly as possible
 				</h2>
 				<div className="intro-posters">
 					<img src={require(`../images/ToanPoster.png`)} alt="" />
 					<img src={require(`../images/VelvetPoster.png`)} alt="" />
 					<img src={require(`../images/TommyVercettiPoster.png`)} alt="" />
 				</div>
-				<button onClick={props.startGame}>Start Game</button>
+				<button className="start-button" onClick={props.startGame}>Start Game</button>
 			</div>
 		</div>
 	)
