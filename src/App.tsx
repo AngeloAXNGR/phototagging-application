1e1955183e878a621aa2c49286833b17e4c34308
Angelo Santos <axngrsantos@gmail.com>
2023-01-20 17:27:40 +0800

    Create score component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import PosterImage from './images/poster.webp'
 import HitBox from './components/HitBox';
 import characters from './data/characters.json'
 import Intro from './components/Intro';
+import Score from './components/Score';
 
+import { db } from './firebase';
+import {collection, addDoc} from 'firebase/firestore'
 
 type CharacterType = {
   id:number
@@ -19,11 +22,14 @@ function App() {
   const [data, setData] = useState<CharacterType[]>([])
   const [gameEnd, setGameEnd] = useState(false);
   const [gameStart, setGameStart] = useState(false);
+  const [counter, setCounter] = useState('00:00:00');
+  const [formData, setFormData] = useState({name:''});
+  const [scoreSubmitted, setScoreSubmitted] = useState(false);
+  const scoreCollectionRef = collection(db, 'Scores');
 
   useEffect(() => {
     setData(characters);
   },[])
-
 
   useEffect(()=>{
     const checkAllMarked = (data:CharacterType[]) => {
@@ -35,14 +41,61 @@ function App() {
 
   },[data])
 
+  useEffect(() => {
+		let timer:number;
+		let seconds = 0;
+
+		if(gameStart && !gameEnd){
+			timer = window.setInterval(() => {
+				seconds +=1
+				const date = new Date(0);
+				date.setSeconds(seconds);
+				const result = date.toISOString().slice(11, 19);
+				setCounter(result);
+			}, 1000)
+		}
+		return () => {
+			clearInterval(timer);
+		}
+	}, [gameStart, gameEnd])
+
+  
+  const startGame = () => {
+    setGameStart(true);
+  }
+
   const markCharacter = (id:number, name:string) => {
     setData(prevData => {
       return prevData.map(character => {
         return character.id === id ? {...character, isMarked:true} : character;
       })
     })
-
   }
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const {name, type, checked, value} = event.target;
+    setFormData(prevData => {
+      return{...prevData,
+        [name]: type === 'checkbox' ? checked : value
+      }
+    })
+  }
+
+  const submitScore = () =>{
+    addDoc(scoreCollectionRef, {score: counter, name: formData.name})
+
+    setScoreSubmitted(true);
+  }
+
+  const resetGame = () =>{
+    setData(characters);
+    setGameStart(false);
+    setGameEnd(false);
+    setScoreSubmitted(false);
+    setFormData({name:''})
+    setCounter('00:00:00')
+  }
+
   const boxElements = data.map(character => {
     return(
       <HitBox
@@ -52,28 +105,32 @@ function App() {
         YCoordinate={character.YCoords}
         isMarked={character.isMarked}
         markCharacter={() => markCharacter(character.id, character.name)}
+        
       />
     );
   })
-
-
-  const startGame = () => {
-    setGameStart(true);
-  }
-
+  
   return(
     <div>
       {gameEnd === false ? <div className='app'>
         {!gameStart && <Intro
           startGame={startGame}
         />}
-        
         <Header
           characterData={data}
+          counter={counter}
         />
         <img src={PosterImage} alt="" width="100%" id="poster"/>
         {boxElements}
-      </div> : <div>Game Over</div>}
+      </div> : 
+      <Score
+        formData={formData}
+        score={counter}
+        handleChange={handleChange}
+        handleSubmit={submitScore}
+        resetGame={resetGame}
+        scoreSubmitted={scoreSubmitted}
+      />}
     </div>
   );
 
---
+++ src/components/Score.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react'
+import { db } from '../firebase'
+import {collection, onSnapshot} from 'firebase/firestore'
+
+type ScoreType = {
+	formData: {name:string}
+	score:string
+	handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+	handleSubmit: (event: React.MouseEvent<HTMLButtonElement>) => void
+	resetGame:(event: React.MouseEvent<HTMLButtonElement>) => void
+	scoreSubmitted: boolean
+}
+
+
+export default function Score(props:ScoreType) {
+	const [scoreList, setScoreList] = useState<any>([])
+	const scoreCollectionRef = collection(db, 'Scores');
+
+
+	useEffect(() =>{
+    const getScores = async() =>{
+      await onSnapshot(scoreCollectionRef, (scores) =>{
+				const temp = scores.docs.map((score) => ({...score.data()}))
+				const sorted = (temp.sort((a,b) => a.score.localeCompare(b.score)))
+				setScoreList(sorted)
+      })
+    }
+
+    getScores();
+	},[]);
+
+	const scoreData = scoreList.map((score: {name: string , score:string}, index:number) => {
+		return(
+			<tr>
+				<td>{index+1}</td>
+				<td>{score.name}</td>
+				<td>{score.score}</td>
+			</tr>
+		);
+	})
+
+
+	return (
+		<div className="leaderboards">
+			{!props.scoreSubmitted ? 
+				<div className="score-form">
+					<h1>You finished in {props.score}</h1>
+					<input
+						type="text"
+						name="name"
+						placeholder="Enter Your Name"
+						value={props.formData.name}
+						onChange={(e) => {props.handleChange(e)}}
+					/>
+					<button onClick={props.handleSubmit} className="submit-score">Submit Score</button>
+				</div> 
+				: 
+				<div className="score-form">
+					<h1>Thank you for Playing!</h1>
+					<button onClick={props.resetGame} className="try-again">Try Again</button>
+				</div>
+			}
+				<div className="player-scores">
+					<table>
+						<tr>
+							<th>Rank</th>
+							<th>Name</th>
+							<th>Time</th>
+						</tr>
+						{scoreData}
+					</table>
+				</div>
+		</div>
+	)
+}
